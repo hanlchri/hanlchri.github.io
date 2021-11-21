58e296491cd5960b4dfb139a4f3c3bf0f4b0df37
23swalisha <ishanswali9@gmail.com>
2021-11-20 22:56:52 -0500

    Bug fixes
    Responsiveness and search framework on its way

--- searchscript.js
+++ searchscript.js
@@ -1,6 +1,5 @@
 // getting all required elements
 const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
 const suggBox = searchWrapper.querySelector(".autocom-box");
 const icon = searchWrapper.querySelector(".icon");
 let linkTag = searchWrapper.querySelector("a");
@@ -8,15 +7,11 @@ let webLink;
 
 // if user press any key and release
 inputBox.onkeyup = (e)=>{
+
     let userData = e.target.value; //user enetered data
     let emptyArray = [];
     if(userData){
-        icon.onclick = ()=>{
-            webLink = "https://www.google.com/search?q=" + userData;
-            linkTag.setAttribute("href", webLink);
-            console.log(webLink);
-            linkTag.click();
-        }
+
         emptyArray = suggestions.filter((data)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
@@ -38,6 +33,7 @@ inputBox.onkeyup = (e)=>{
 }
 
 function select(element){
+
     let selectData = element.textContent;
     inputBox.value = selectData;
     icon.onclick = ()=>{
