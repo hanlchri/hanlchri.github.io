8c4101f418d47051b1cb41fbf71c96995307506f
23swalisha <ishanswali9@gmail.com>
2022-01-12 22:31:28 -0500

    Submenus added

---
+++ APCSscript.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#assign2").click(function () {
+        $("#Assign2Links").toggle();
+    })
+    $("#words").click(function () {
+        $("#Assign2Links").hide();
+    })
+});
